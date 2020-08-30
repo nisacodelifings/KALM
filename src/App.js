e5c53c9dcf4d4c82d75cbea9aaad419d0ba2ce3b
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-08-29 20:12:21 -0700

    add webpack

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-const [name, setName] = React.useState("Annisa")
+  const [name, setName] = React.useState("Annisa");
+  const [happen, setHappen] = React.useState("Happen");
+
   return (
     <div className="App">
       <h1>Hello {name}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={() => setName('Whoever you')}>
-        Update the Title!
+      <h2>Start editing to see some magic {happen}!</h2>
+      <button onClick={() => setName("Whoever you")}>Update the Title!</button>
+      <button onClick={() => setHappen(<a href="#">Great!</a>)}>
+        Update the Happen!
       </button>
     </div>
   );
