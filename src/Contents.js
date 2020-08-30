d20fe87f8f382433f77bf650aaf87b68c7173f87
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-08-30 00:01:26 -0700

    new style!

--- src/Contents.js
+++ src/Contents.js
@@ -8,13 +8,13 @@ export default function Contents() {
     <div className="Contents">
       <ul>
         <li>
-          <a onClick={() => setIsi("ini Dashboard")}>Dashboard</a>
+          <a onClick={() => setIsi(<p>ini Dashboard</p>)}>Dashboard</a>
         </li>
         <li>
-          <a onClick={() => setIsi("ini About")}>About</a>
+          <a onClick={() => setIsi(<p>ini About</p>)}>About</a>
         </li>
         <li>
-          <a onClick={() => setIsi("ini Contact")}>Contact</a>
+          <a onClick={() => setIsi(<p>ini Contact</p>)}>Contact</a>
         </li>
       </ul>
       <p>{isi}</p>
