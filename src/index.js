41446ba7f2155981138ffdbe09fbd417f3906415
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-08-29 19:32:56 -0700

    destructuring {render} react-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
 import App from "./App";
 import Menu from "./Menu";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
     <Menu />
